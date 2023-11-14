107851a8234b644312e16e6ce7e863127551c1a8
Emma Gerigscott <elgerig@gmail.com>
2023-11-14 09:03:25 -0800

    Add allergen constants to allergies.js

--- allergies.js
+++ allergies.js
@@ -1,7 +1,13 @@
-//
-// This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const ALLERGENS = {
+  1: 'eggs',
+  2: 'peanuts',
+  4: 'shellfish',
+  8: 'strawberries',
+  16: 'tomatoes',
+  32: 'chocolate',
+  64: 'pollen',
+  128: 'cats',
+}
 
 export class Allergies {
   constructor() {
