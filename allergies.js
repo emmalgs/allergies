b414054dce56a27efc3cb048261300329bc88952
Emma Gerigscott <elgerig@gmail.com>
2023-11-21 11:09:47 -0800

    WIP: test for allergen parts not passing

--- allergies.js
+++ allergies.js
@@ -1,13 +1,15 @@
+import { _ } from "core-js";
+
 const ALLERGENS = {
-  1: 'eggs',
-  2: 'peanuts',
-  4: 'shellfish',
-  8: 'strawberries',
-  16: 'tomatoes',
-  32: 'chocolate',
-  64: 'pollen',
-  128: 'cats',
-}
+  1: "eggs",
+  2: "peanuts",
+  4: "shellfish",
+  8: "strawberries",
+  16: "tomatoes",
+  32: "chocolate",
+  64: "pollen",
+  128: "cats",
+};
 
 export class Allergies {
   constructor(score) {
@@ -20,8 +22,10 @@ export class Allergies {
     let allergenKeys = Object.keys(ALLERGENS).reverse();
     for (let i = 0; i < allergenKeys.length; i++) {
       if (allergenScore >= allergenKeys[i]) {
+        if (allergenScore % allergenKeys[i] === 0) {
         this.allergens.unshift(ALLERGENS[allergenKeys[i]]);
         allergenScore -= allergenKeys[i];
+        }
       }
     }
     return this.allergens;
